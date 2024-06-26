bfc2dea8ac1824798d6b1a604987c151c736954a
aankor <aankor@DESKTOP-B6QHAKJ>
2024-06-26 13:38:07 +0200

    Step

--- ts/sdk/vite.config.ts
+++ ts/sdk/vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig, mergeConfig } from 'vite'
-import { tanstackBuildConfig } from '@tanstack/config/build'
+/* eslint-disable node/no-unpublished-import */
+import {defineConfig, mergeConfig} from 'vite';
+import {tanstackBuildConfig} from '@tanstack/config/build';
 
 const config = defineConfig({
   // Framework plugins, vitest config, etc.
-})
+});
 
 export default mergeConfig(
   config,
   tanstackBuildConfig({
     entry: './src/index.ts',
     srcDir: './src',
-  }),
-)
+  })
+);
